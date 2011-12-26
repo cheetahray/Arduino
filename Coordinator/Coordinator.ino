7976d18e158ce8a9574ae7fe32957031af40b47f
cheetahray <ray608@gmail.com>
2011-12-26 09:26:31 +0800

    關美

---
+++ Coordinator/Coordinator.ino
@@ -0,0 +1,31 @@
+/*
+   EXcode 15-1
+*/
+int ledPin = 13; 
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if(Serial.available() > 0)  
+    {
+      char D1 = Serial.read();
+      if('A' == D1)  //確認起始字元
+        {
+         Serial.write('A');
+       }
+       /*
+       else if('B' == D1)  //確認起始字元
+        {
+         Serial.write('B');
+       }
+       */
+    }    
+
+   delay(100); 
+}
