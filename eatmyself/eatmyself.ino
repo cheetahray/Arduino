ac4b78f31d7ee2dcbab938ddf35da406ebb722ca
ray <ray@dac.tw>
2016-04-06 19:39:00 +0800

    丟包率大概 50%

---
+++ eatmyself/eatmyself.ino
@@ -0,0 +1,137 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+
+// wifi connection variables
+const char* ssid = "rayray";
+const char* password = "pearl123";
+boolean wifiConnected = false;
+
+// UDP variables
+unsigned int localPort = 8888;
+WiFiUDP UDP;
+boolean udpConnected = false;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged"; // a string to send back
+
+void setup() {
+  // Initialise Serial connection
+  Serial.begin(115200);
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  
+  // Initialise wifi connection
+  wifiConnected = true; //connectWifi();
+
+  // only proceed if wifi connection successful
+  if (wifiConnected) {
+    udpConnected = connectUDP();
+    if (udpConnected) {
+      // initialise pins
+      pinMode(5, OUTPUT);
+    }
+  }
+}
+
+void loop() {
+  // check if the WiFi and UDP connections were successful
+  if (wifiConnected) {
+    if (udpConnected) {
+
+      // if there’s data available, read a packet
+      int packetSize = UDP.parsePacket();
+      if (packetSize)
+      {
+        Serial.println("");
+        Serial.print("Received packet of size ");
+        Serial.println(packetSize);
+        Serial.print("From ");
+        IPAddress remote = UDP.remoteIP();
+        for (int i = 0; i < 4; i++)
+        {
+          Serial.print(remote[i], DEC);
+          if (i < 3)
+          {
+            Serial.print(".");
+          }
+        }
+        Serial.print(", port ");
+        Serial.println(UDP.remotePort());
+
+        // read the packet into packetBufffer
+        UDP.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        Serial.println("Contents: ");
+        int value = packetBuffer[0] * 256 + packetBuffer[1];
+        Serial.println(value);
+
+        // send a reply, to the IP address and port that sent us the packet we received
+        //UDP.beginPacket(UDP.remoteIP(), 12345); //UDP.remotePort());
+        //UDP.write(ReplyBuffer);
+        //UDP.endPacket();
+
+        // turn LED on or off depending on value recieved
+        //digitalWrite(5, value);
+      }
+      delay(100);
+
+    }
+
+  }
+
+}
+
+// connect to UDP – returns true if successful or false if not
+boolean connectUDP() {
+  boolean state = false;
+
+  Serial.println("");
+  Serial.println("Connecting to UDP");
+
+  if (UDP.begin(localPort) == 1) {
+    Serial.println("Connection successful");
+    state = true;
+  }
+  else {
+    Serial.println("Connection failed");
+  }
+
+  return state;
+}
+
+/*
+// connect to wifi – returns true if successful or false if not
+boolean connectWifi() {
+  boolean state = true;
+  int i = 0;
+  WiFi.begin(ssid); //, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 10) {
+      state = false;
+      break;
+    }
+    i++;
+  }
+  if (state) {
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("");
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
+*/
